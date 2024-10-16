e78aafaeefa637e88c0aa9cf0f2fc1dbff7e3178
toth tamas <toth.tamas@diaok.szbi-pg.hu>
2024-10-16 09:25:48 +0200

    added division

--- test_calculator_service.py
+++ test_calculator_service.py
@@ -35,3 +35,12 @@ def test_calculate_multiplication_with_mock_verifies_get_data_called():
     
     assert result == 12         # 2 * 2 * 3 = 12
     mock_repository.get_data.assert_called_once()
+def test_calculate_division_with_mock_verifies_get_data_called():
+    mock_repository = Mock(IDataRepository)
+    mock_repository.get_data.return_value = 3
+
+    calculator_service = CalculatorService(mock_repository)
+    result = calculator_service.calculate_division(12, 4)
+    
+    assert result == 2         # 12 / 4 / 3 = 2
+    mock_repository.get_data.assert_called_once()
